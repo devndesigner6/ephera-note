2eb99eb3792d0e26f1424eebd0488df533a43009
devndesigner6 <peddadahemanth6@gmail.com>
2026-01-06 18:37:56 +0530

    chore: skip social cover in PWA precache

--- vite.config.ts
+++ vite.config.ts
@@ -57,6 +57,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,svg,png,wasm}"],
         maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MiB
+        globIgnores: ["**/readme-cover.png"],
         navigateFallback: "index.html",
         navigateFallbackDenylist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
         runtimeCaching: [
